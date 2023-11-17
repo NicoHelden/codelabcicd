00735aefd9c85a7160a678e558abecf60a07b45a
nicolas.heldenbergh@gmail.com <nicolas.heldenbergh@gmail.com>
2023-11-17 09:35:07 +0100

    1

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,5 +1,19 @@
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 
-class MainTest {
+@QuarkusTest
+public class MainTest {
 
+    @Test
+    public void testHelloEndpoint() {
+        RestAssured.given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("Hi there!"));
+    }
 }
